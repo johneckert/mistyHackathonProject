dd13fd62b8023d97f1b2ad8b55bfde276652f7ea
Alex Guevara <a.guevara23@gmail.com>
2018-06-20 10:56:34 -0400

    test skill recog

--- Skill-Face Recognition/app.js
+++ Skill-Face Recognition/app.js
@@ -1,4 +1,3 @@
-
 var connect = document.getElementById("connect");
 var ipAddress = document.getElementById("ip-address");
 var resultsBox = document.getElementById("results");
@@ -8,12 +7,12 @@ var stop = document.getElementById("stop");
 var client;
 var ip;
 var msg = {
-  "$id": "1",
-  "Operation": "subscribe",
-  "Type": "FaceRecognition",
-  "DebounceMs": 100,
-	"EventName": "FaceRecognition",
-  "Message": ""
+  $id: "1",
+  Operation: "subscribe",
+  Type: "FaceRecognition",
+  DebounceMs: 100,
+  EventName: "FaceRecognition",
+  Message: ""
 };
 var message = JSON.stringify(msg);
 var messageCount = 0;
@@ -46,28 +45,30 @@ stop.onclick = function() {
 };
 
 function startFaceRecognition() {
-    //Create a new websocket
-    socket = new WebSocket("ws://" + ip + "/pubsub");
-    //When the socket is open, send the message
-    socket.onopen = function(event) {
-      printToScreen("WebSocket opened.");
-      socket.send(message);
-      client.PostCommand("beta/faces/recognition/start", null, handleResult);
-    };
-    // Handle messages received from the server
-    socket.onmessage = function(event) {
-      console.log(event);
-      var message = JSON.parse(event.data).message;
-      console.log(message);
-      if (message.personName && message.personName !== "unknown person") {
-        printToScreen("I think I know you. Is this " + message.personName + "?");
-      } else {
-        messageCount += 1;
-      }
+  //Create a new websocket
+  socket = new WebSocket("ws://" + ip + "/pubsub");
+  //When the socket is open, send the message
+  socket.onopen = function(event) {
+    printToScreen("WebSocket opened.");
+    socket.send(message);
+    client.PostCommand("beta/faces/recognition/start", null, handleResult);
+  };
+  // Handle messages received from the server
+  socket.onmessage = function(event) {
+    console.log("[onmessage] event", event);
+    var message = JSON.parse(event.data).message;
+    console.log("[onmessage] message:", message);
+    if (message.personName && message.personName !== "unknown person") {
+      printToScreen("I think I know you. Is this " + message.personName + "?");
+    } else {
+      messageCount += 1;
+      printToScreen("An unknown face detected");
+      // message.personName = "Alex";
+
       if (messageCount % 10 === 0) {
         printToScreen("Looking for a familiar face ...");
       }
-    };
+    }
     // Handle any errors that occur.
     socket.onerror = function(error) {
       console.log("WebSocket Error: " + error);
@@ -76,30 +77,30 @@ function startFaceRecognition() {
     socket.onclose = function(event) {
       printToScreen("WebSocket closed.");
     };
-};
-
+  };
+}
 function handleResult(data) {
   console.log(data);
   if (data[0].result) {
-    printToScreen("Face recognition started.")
+    printToScreen("Face recognition has started.");
   }
 }
 
 function validateIPAddress(ip) {
-	var ipNumbers = ip.split(".");
-	var ipNums = new Array(4);
-	if (ipNumbers.length !== 4) {
-		return "";
-	}
-	for (let i = 0; i < 4; i++) {
-		ipNums[i] = parseInt(ipNumbers[i]);
-		if (ipNums[i] < 0 || ipNums[i] > 255) {
-			return "";
-		}
-	}
-	return ip;
-};
+  var ipNumbers = ip.split(".");
+  var ipNums = new Array(4);
+  if (ipNumbers.length !== 4) {
+    return "";
+  }
+  for (let i = 0; i < 4; i++) {
+    ipNums[i] = parseInt(ipNumbers[i]);
+    if (ipNums[i] < 0 || ipNums[i] > 255) {
+      return "";
+    }
+  }
+  return ip;
+}
 
 function printToScreen(msg) {
-  resultsBox.innerHTML += (msg + "\r\n");
+  resultsBox.innerHTML += msg + "\r\n";
 }
